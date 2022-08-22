68c4dcee43f1002fb92e00bd9eb683dd5a812236
tonylf <tonylonefight@gmail.com>
2022-08-22 10:09:56 -0500

    got JD help to get the connection working

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
@@ -7,9 +6,9 @@ const sequelize = process.env.JAWSDB_URL
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      // dialectOptions: {
+      //   decimalNumbers: true,
+      // },
     });
 
 module.exports = sequelize;
